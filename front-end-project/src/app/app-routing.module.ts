1fab961a14adb07cc0b7e5ac441865915428c0b7
lucaswiix <lucas.rocha@usto.re>
2019-04-13 17:53:32 -0300

    fixed

--- front-end-project/src/app/app-routing.module.ts
+++ front-end-project/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
+// import { LoginComponent } from './login/login.component';
 
 
 const routes: Routes = [
@@ -133,7 +133,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { useHash: true })],
+  imports: [RouterModule.forRoot(routes, { useHash: false })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
